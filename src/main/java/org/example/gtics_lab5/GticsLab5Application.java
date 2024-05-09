13a63d960589ac1c2bf1e3162b9dbed77d2972c9
Ana <anapaula.hachi@hotmail.com>
2024-05-08 19:12:45 -0500

    first commit

---
+++ src/main/java/org/example/gtics_lab5/GticsLab5Application.java
@@ -0,0 +1,13 @@
+package org.example.gtics_lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab5Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gtics_lab5/GticsLab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gtics_lab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
